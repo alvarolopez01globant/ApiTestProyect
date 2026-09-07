1a927e1b3c2b7a3a5288fd6ff6ae2e1192d545bf
alvaro.lopez01 <alvaro.lopez01@globant.com>
2026-09-07 11:14:50 -0500

    making base url

--- src/test/java/org/globant/utils/BaseTest.java
+++ src/test/java/org/globant/utils/BaseTest.java
@@ -1,5 +1,23 @@
 package org.globant.utils;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.globant.configuration.ConfigurationManager;
+import org.testng.annotations.BeforeSuite;
+
 public class BaseTest {
+
+    @BeforeSuite
+    public void setupSuite() {
+        RestAssured.baseURI = ConfigurationManager.getBaseUrl();
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .build();
+    }
 
 }
