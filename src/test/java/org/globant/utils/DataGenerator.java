a172fc9fe4f18804839d94668cdbcd7437fe85c3
alvaro.lopez01 <alvaro.lopez01@globant.com>
2026-09-07 11:12:27 -0500

    making data generator

--- src/test/java/org/globant/utils/DataGenerator.java
+++ src/test/java/org/globant/utils/DataGenerator.java
@@ -1,4 +1,20 @@
 package org.globant.utils;
 
+import org.globant.models.User;
+
 public class DataGenerator {
+
+    public static User generateRandomUser() {
+        long timestamp = System.currentTimeMillis();
+        return User.builder()
+                .id(timestamp)
+                .username("user_" + timestamp)
+                .firstName("Perf")
+                .lastName("Dog")
+                .email("test_" + timestamp + "@perfdog.com")
+                .password("Pass1234!")
+                .phone("123456789")
+                .userStatus(1)
+                .build();
+    }
 }
